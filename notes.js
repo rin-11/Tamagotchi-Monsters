12e340db2c1dddf9e75d810579c4f9aed8415c37
Rin Kates <erinkates11@gmail.com>
2023-02-03 02:52:59 -0500

    metric functions working

--- notes.js
+++ notes.js
@@ -1,11 +1,19 @@
 
 // STEPS
-    // 1. Add four moster phase sprite classes
-    // 2. Add game timer
-    // 3. Set timer to show which monster phase at certain interval
-    // 4. Add hunger, health, abilities metric progress bars
-    // 5. Create feed, sleep, train button linked to metrics
+    // -- Add four monster phase sprite classes
 
+    // -- Add game timer
+
+
+    // -- Create function that changes monster sprite showing at different times
+    
+    
+    // -- Add hunger, energy, power metric progress bar
+            // create function that decreases hunger bar every 5 seconds
+
+    // -- Create eat, sleep, train button linked to metrics
+            // use onclick in HTML buttons to link to js function
+            // function to increase power bar only works if hunger < 70 % && energy > 70%
 
 
 
